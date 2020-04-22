3b67a933bf5c3a9f50f6183c784ed55648ecd641
Joanna Preston <joannappreston@gmail.com>
2020-04-22 15:56:43 -0700

    CSS styling and updated About Me section

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// event listener for hamburger drop down menu
+$(".navbar-burger").each(function () {
+    $(this).on("click", function () {
+        // targets data-target attribute which is equal to regular nav bar menu's ID
+        var targetAttribute = $(this).attr("data-target");
+        var $navbarMenuID = $("#" + targetAttribute);
+
+        // toggles active class between regular navbar menu and hamburger menu
+        $(this).toggleClass("is-active");
+        $navbarMenuID.toggleClass("is-active");
+    });
+});
